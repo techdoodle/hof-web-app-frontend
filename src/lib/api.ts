cb16f7ac42507aca82b142cc5761e39af6136841
Daksh Taneja <daksh@plutus.gg>
2025-07-26 14:43:42 +0530

    fixing env vars

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getAccessToken, getRefreshToken, storeAuthData, clearAuthData } from './utils/auth';
 
 const api = axios.create({
-  baseURL: process.env.API_BASE_URL || 'http://localhost:8000',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000',
 });
 
 api.interceptors.request.use(config => {
