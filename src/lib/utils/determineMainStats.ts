424320fa29226c1ecbf9b1d9fcf37d6d233f5f1a
techdoodle <p.gambhir09@gmail.com>
2025-08-26 21:12:51 +0530

    fix: takles in def profile now showing

--- src/lib/utils/determineMainStats.ts
+++ src/lib/utils/determineMainStats.ts
@@ -4,7 +4,7 @@ export const determineMainStats = (playerPosition: 'GK' | 'DEF' | 'FWD', matchSt
         case 'GK':
             return { 'Saves': matchStats?.detailedStats?.goalkeeping?.totalSave ?? "-", 'Key Passes': matchStats?.detailedStats?.passing?.totalKeyPass ?? "-" };
         case 'DEF':
-            return { 'Tackles': matchStats?.detailedStats?.tackling?.totalTackleAttempts ?? "-", 'Assists': matchStats?.detailedStats?.impact?.totalAssists ?? "-" };
+            return { 'Tackles': matchStats?.detailedStats?.tackling?.totalTackles ?? "-", 'Assists': matchStats?.detailedStats?.impact?.totalAssists ?? "-" };
         case 'FWD':
             return { 'Goals': matchStats?.detailedStats?.impact?.totalGoals ?? "-", 'Assists': matchStats?.detailedStats?.impact?.totalAssists ?? "-" };
     }
