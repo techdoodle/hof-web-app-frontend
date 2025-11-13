85a0c73da5e6720848fe4a63e7b0055bcca36690
control-alt-defeat-31 <daksh.taneja.hof@gmail.com>
2025-11-13 12:49:47 +0530

    fixing icon path

--- src/components/common/CommonNavbar.tsx
+++ src/components/common/CommonNavbar.tsx
@@ -20,8 +20,8 @@ export function CommonNavbar({ activeTab }: CommonNavbarProps) {
         label: 'Home',
         href: '/home',
         iconSrc: {
-          active: '/icons/nav-new/Home-active.svg',
-          inactive: '/icons/nav-new/Home-inactive.svg',
+          active: '/icons/nav/Home-active.svg',
+          inactive: '/icons/nav/Home-inactive.svg',
         },
       },
       {
